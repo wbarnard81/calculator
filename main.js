a7392f224b4a9b72312d8d88e5e77560bd83415f
Werner Barnard <wbarnard81@gmail.com>
2018-08-23 10:23:19 +0200

    Functionality improved

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 var displayArray = [];
 var mathButton = [];
+var mathArray = [];
+var firstSet = [];
+var mathGroup = [];
 
 window.onload = function () {
     document.getElementById("calcText").value = 0;
@@ -9,22 +12,28 @@ function isNumber(displayArray) {
     return !isNaN(parseFloat(displayArray)) && isFinite(displayArray);
 }
 
+function getReady() {
+    firstSet = displayArray.filter(isNumber);
+    displayArray = [];
+    mathArray = firstSet.join('');
+    mathGroup.push(mathArray);
+    mathArray =[];
+    mathGroup.map(function(item) {
+        return parseInt(item);
+    });
+}
+
 function updateDisplay(id){
     var numButton = id.innerHTML;
     displayArray.push(numButton);
-    document.getElementById("calcText").value = displayArray.join(' ');
-};
-
-function updateZero(id){
-    var numButton = id.innerHTML;
-    displayArray.push(numButton);
-    document.getElementById("calcText").value = displayArray.join(' ');
+    document.getElementById("calcText").value = displayArray.join('');
 };
 
 function mathDisplay(id) {
     mathButton = id.innerHTML;
     displayArray.push(mathButton);
-    document.getElementById("calcText").value = displayArray.join(' ');
+    document.getElementById("calcText").value = displayArray.join('');
+    getReady();
 }
 
 function clearDisplay(){
@@ -33,24 +42,26 @@ function clearDisplay(){
 }
 
 $('#equal').on('click', function() {
-    var validNumbers = displayArray.filter(isNumber);
+    //var validNumbers = displayArray.filter(isNumber);
+    getReady();
     if (mathButton == '+') {
-        var sum = validNumbers.reduce(function (a, b) {
+        var sum = mathGroup.reduce(function (a, b) {
                 return Number(a) + Number(b)
             });
     } else if (mathButton == '-'){
-        var sum = validNumbers.reduce(function (a, b) {
-                return a - b
+        var sum = mathGroup.reduce(function (a, b) {
+                return Number(a) - Number(b)
             });
     } else if (mathButton == '*'){
-        var sum = validNumbers.reduce(function (a, b) {
-                return a * b
+        var sum = mathGroup.reduce(function (a, b) {
+                return Number(a) * Number(b)
             });
     } else if (mathButton == '/'){
-        var sum = validNumbers.reduce(function (a, b) {
-                return a / b
+        var sum = mathGroup.reduce(function (a, b) {
+                return Number(a) / Number(b)
             });
     }
     document.getElementById("calcText").value = sum;
-    displayArray = [sum];
+    displayArray = [];
+    mathGroup = [];
 });
